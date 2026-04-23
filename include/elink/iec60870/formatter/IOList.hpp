16f8e55663cb803473fbc2ff89d6ccea59562a45
BlueRabbitY <BlueRabbitY@protonmail.com>
2026-04-24 00:10:43 +0800

    refactor: refactor io header file
    Signed-off-by: BlueRabbitY <BlueRabbitY@protonmail.com>

---
+++ include/elink/iec60870/formatter/IOList.hpp
@@ -0,0 +1,85 @@
+/***********************************************************************************
+ * \file IOList.hpp
+ * \author BlueRabbitY (BlueRabbitY\@protonmail.com)
+ * \brief 
+ * \date 2026-04-24 00:07:26
+ * 
+ * \copyright Copyright (C) 2026-2026 BlueRabbitY. All rights reserved.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ ***********************************************************************************/
+#pragma once
+
+#include "elink/iec60870/io/InformationObject.hpp"
+#include "elink/iec60870/io/SinglePointInformation.hpp"
+#include "elink/iec60870/io/SinglePointWithCP24Time2a.hpp"
+#include "elink/iec60870/io/DoublePointInformation.hpp"
+#include "elink/iec60870/io/DoublePointWithCP24Time2a.hpp"
+#include "elink/iec60870/io/StepPositionInformation.hpp"
+#include "elink/iec60870/io/StepPositionWithCP24Time2a.hpp"
+#include "elink/iec60870/io/BitString32.hpp"
+#include "elink/iec60870/io/BitString32WithCP24Time2a.hpp"
+#include "elink/iec60870/io/MeasuredValueNormalized.hpp"
+#include "elink/iec60870/io/MeasuredValueNormalizedWithCP24Time2a.hpp"
+#include "elink/iec60870/io/MeasuredValueScaled.hpp"
+#include "elink/iec60870/io/MeasuredValueScaledWithCP24Time2a.hpp"
+#include "elink/iec60870/io/MeasuredValueShort.hpp"
+#include "elink/iec60870/io/MeasuredValueShortWithCP24Time2a.hpp"
+#include "elink/iec60870/io/IntegratedTotals.hpp"
+#include "elink/iec60870/io/IntegratedTotalsWithCP24Time2a.hpp"
+#include "elink/iec60870/io/EventOfProtectionEquipment.hpp"
+#include "elink/iec60870/io/PackedStartEventsOfProtectionEquipment.hpp"
+#include "elink/iec60870/io/PackedOutputCircuitInfo.hpp"
+#include "elink/iec60870/io/PackedSinglePointWithSCD.hpp"
+#include "elink/iec60870/io/MeasuredValueNormalizedWithoutQuality.hpp"
+#include "elink/iec60870/io/SinglePointWithCP56Time2a.hpp"
+#include "elink/iec60870/io/DoublePointWithCP56Time2a.hpp"
+#include "elink/iec60870/io/StepPositionWithCP56Time2a.hpp"
+#include "elink/iec60870/io/BitString32WithCP56Time2a.hpp"
+#include "elink/iec60870/io/MeasuredValueNormalizedWithCP56Time2a.hpp"
+#include "elink/iec60870/io/MeasuredValueScaledWithCP56Time2a.hpp"
+#include "elink/iec60870/io/MeasuredValueShortWithCP56Time2a.hpp"
+#include "elink/iec60870/io/IntegratedTotalsWithCP56Time2a.hpp"
+#include "elink/iec60870/io/EventOfProtectionEquipmentWithCP56Time2a.hpp"
+#include "elink/iec60870/io/PackedStartEventsOfProtectionEquipmentWithCP56Time2a.hpp"
+#include "elink/iec60870/io/PackedOutputCircuitInfoWithCP56Time2a.hpp"
+#include "elink/iec60870/io/SingleCommand.hpp"
+#include "elink/iec60870/io/DoubleCommand.hpp"
+#include "elink/iec60870/io/StepCommand.hpp"
+#include "elink/iec60870/io/SetpointCommandNormalized.hpp"
+#include "elink/iec60870/io/SetpointCommandScaled.hpp"
+#include "elink/iec60870/io/SetpointCommandShort.hpp"
+#include "elink/iec60870/io/BitString32Command.hpp"
+#include "elink/iec60870/io/SingleCommandWithCP56Time2a.hpp"
+#include "elink/iec60870/io/DoubleCommandWithCP56Time2a.hpp"
+#include "elink/iec60870/io/StepCommandWithCP56Time2a.hpp"
+#include "elink/iec60870/io/SetpointCommandNormalizedWithCP56Time2a.hpp"
+#include "elink/iec60870/io/SetpointCommandScaledWithCP56Time2a.hpp"
+#include "elink/iec60870/io/SetpointCommandShortWithCP56Time2a.hpp"
+#include "elink/iec60870/io/BitString32CommandWithCP56Time2a.hpp"
+#include "elink/iec60870/io/EndOfInitialization.hpp"
+#include "elink/iec60870/io/InterrogationCommand.hpp"
+#include "elink/iec60870/io/CounterInterrogationCommand.hpp"
+#include "elink/iec60870/io/ReadCommand.hpp"
+#include "elink/iec60870/io/ClockSynchronizationCommand.hpp"
+#include "elink/iec60870/io/TestCommand.hpp"
+#include "elink/iec60870/io/ResetProcessCommand.hpp"
+#include "elink/iec60870/io/DelayAcquisitionCommand.hpp"
+#include "elink/iec60870/io/TestCommandWithCP56Time2a.hpp"
+#include "elink/iec60870/io/ParameterNormalizedValue.hpp"
+#include "elink/iec60870/io/ParameterScaledValue.hpp"
+#include "elink/iec60870/io/ParameterFloatValue.hpp"
+#include "elink/iec60870/io/ParameterActivation.hpp"
+#include "elink/iec60870/io/FileReady.hpp"
+#include "elink/iec60870/io/SectionReady.hpp"
+#include "elink/iec60870/io/FileCallOrSelect.hpp"
+#include "elink/iec60870/io/FileLastSegmentOrSection.hpp"
+#include "elink/iec60870/io/FileACK.hpp"
+#include "elink/iec60870/io/FileSegment.hpp"
+#include "elink/iec60870/io/FileDirectory.hpp"
+#include "elink/iec60870/io/QueryLog.hpp"
